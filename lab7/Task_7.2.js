cf3fe310412d6bb2bdd7e0f037e29d4e0a67e99c
WenHeng123456 <89001623+WenHeng123456@users.noreply.github.com>
2022-10-12 10:18:39 +0800

    Add files via upload

---
+++ lab7/Task_7.2.js
@@ -0,0 +1,52 @@
+function init(){
+    
+	var w = 300;
+    var h = 300;
+	//var padding=10;
+    var data = [5,6,10,20,25,45]
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var outerRadius = w /2;
+    var innerRadius = 0;
+
+    var arc = d3.arc()
+                .outerRadius(outerRadius)
+                .innerRadius(innerRadius);
+
+    var pie = d3.pie();
+            
+                
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    var arcs = svg.selectAll("g.arc")
+                    .data(pie(data))
+                    .enter()
+                    .append("g")
+                    .attr("class","arc")
+                    .attr("transform","translate("+outerRadius+","+outerRadius+")");
+
+    arcs.append("path")
+        .attr("fill", function(d,i){
+            return color(i);
+        })
+        .attr("d",function(d,i){
+            return arc(d,i);
+        });
+    
+    arcs.append("text")
+        .text(function(d){
+            return d.value;
+        })
+        .attr("transform", function(d){
+            return "translate(" + arc.centroid(d)+ ")";
+        })
+
+
+
+} 
+        
+	
+window.onload= init;
